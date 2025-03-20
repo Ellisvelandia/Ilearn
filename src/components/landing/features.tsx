4426ffa26558a775b07c9ec3636076ac9d81330b
ellis  velandia caicedo <100318892+Ellisvelandia@users.noreply.github.com>
2025-03-20 01:34:58 -0500

    feat: simplify FeaturesSection component; remove unnecessary section wrapper for cleaner structure

--- src/components/landing/features.tsx
+++ src/components/landing/features.tsx
@@ -3,9 +3,5 @@
 import { Features } from "./features/index";
 
 export function FeaturesSection() {
-  return (
-    <section id="features" className="py-20 sm:py-24 lg:py-32">
-      <Features />
-    </section>
-  );
+  return <Features />;
 }
